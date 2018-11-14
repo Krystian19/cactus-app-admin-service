e29df9b89725442087face7a859c2a9554355b8d
Jan Guzman <janfrancisco19@gmail.com>
2018-11-14 14:51:55 -0400

    Removing tracked files + Connecting to db instance

--- cactus_backend_admin/settings.py
+++ cactus_backend_admin/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'cactus_backend_admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cactus_app',
+        'USER': 'root',
+        'PASSWORD': 'secret',
+        'HOST': 'mysql',
+        'PORT': '3306',
     }
 }
 
