a9e2f8e2ad7f8d0ed1d1ea5a90669da154f97bde
Jan Guzman <janfrancisco19@gmail.com>
2018-11-15 23:11:45 -0400

    Working with the model allignment

--- Anime/admin.py
+++ Anime/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Anime
+from .models import Anime, Genre, AnimeGenre
 
 # Register your models here.
 admin.site.register(Anime)
+admin.site.register(Genre)
+admin.site.register(AnimeGenre)
--- Anime/models.py
+++ Anime/models.py
@@ -10,3 +10,28 @@ class Anime(models.Model):
 
     def __str__(self):
         return self.title
+
+class Genre(models.Model):
+    title = models.CharField(max_length=250)
+    thumbnail = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'Genres'
+
+    def __str__(self):
+        return self.title
+
+# This is how Anime and Genres are related ...
+class AnimeGenre(models.Model):
+    anime_id = models.ForeignKey(Anime, on_delete=models.CASCADE, db_column='anime_id')
+    genre_id = models.ForeignKey(Genre, on_delete=models.CASCADE, db_column='genre_id')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'AnimeGenres'
+
+    def __str__(self):
+        return " anime_id: " + str(self.anime_id) + " ,genre_id: " + str(self.genre_id)
