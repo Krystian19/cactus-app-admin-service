f08483905b3012abcad6cf299ae8d87651c044e4
Jan Guzman <janfrancisco19@gmail.com>
2018-11-16 10:06:18 -0400

    Adding missing models

--- Anime/models.py
+++ Anime/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+
+class Language(models.Model):
+    name = models.CharField(max_length=250)
+    iso_code = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'Languages'
+
+    def __str__(self):
+        return self.name + " " + iso_code
+
+
 class Anime(models.Model):
     title = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,6 +24,7 @@ class Anime(models.Model):
 
     def __str__(self):
         return self.title
+
 
 class Genre(models.Model):
     title = models.CharField(max_length=250)
@@ -23,10 +38,14 @@ class Genre(models.Model):
     def __str__(self):
         return self.title
 
-# This is how Anime and Genres are related ...
 class AnimeGenre(models.Model):
-    anime_id = models.ForeignKey(Anime, on_delete=models.CASCADE, db_column='anime_id')
-    genre_id = models.ForeignKey(Genre, on_delete=models.CASCADE, db_column='genre_id')
+    """
+    This is how Anime and Genres are related ...
+    """
+    anime_id = models.ForeignKey(
+        Anime, on_delete=models.CASCADE, db_column='anime_id')
+    genre_id = models.ForeignKey(
+        Genre, on_delete=models.CASCADE, db_column='genre_id')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -35,3 +54,42 @@ class AnimeGenre(models.Model):
 
     def __str__(self):
         return " anime_id: " + str(self.anime_id) + " ,genre_id: " + str(self.genre_id)
+
+class AnimeDescription(models.Model):
+    """
+    Animes have multiple descriptions depending of the language is available
+    """
+    anime_id = models.ForeignKey(
+        Anime, on_delete=models.CASCADE, db_column='anime_id')
+    language_id = models.ForeignKey(
+        Language, on_delete=models.CASCADE, db_column='language_id')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'AnimeDescriptions'
+
+    def __str__(self):
+        return self.name + " " + iso_code
+
+class Season(models.Model):
+    """
+    Anime series tend to have multiple seasons
+    """
+    anime_id = models.ForeignKey(
+        Anime, on_delete=models.CASCADE, db_column='anime_id')
+    seasonOrder = models.IntegerField()
+    title = models.CharField(max_length=250)
+    startedAiring = models.DateTimeField()
+    stoppedAiring = models.DateTimeField()
+    poster = models.CharField(max_length=250)
+    background = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'Seasons'
+
+    def __str__(self):
+        return self.name + " " + iso_code
