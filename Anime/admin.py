e99388cc62702e9a3ff28f8420567e89943baf7f
Jan Guzman <janfrancisco19@gmail.com>
2018-11-17 22:26:44 -0400

    Including missing models into the admin's panel

--- Anime/admin.py
+++ Anime/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Anime, Genre, AnimeGenre, AnimeDescription, Season, SeasonAlternativeTitle, Language
+from .models import Anime
+from .models import Genre
+from .models import AnimeGenre
+from .models import AnimeDescription
+from .models import Season
+from .models import SeasonAlternativeTitle 
+from .models import Language
+from .models import Episode
+from .models import EpisodeTitle
+from .models import EpisodeVersion
 
 # Register your models here.
 admin.site.register(Language)
@@ -9,3 +18,6 @@ admin.site.register(AnimeGenre)
 admin.site.register(AnimeDescription)
 admin.site.register(Season)
 admin.site.register(SeasonAlternativeTitle)
+admin.site.register(Episode)
+admin.site.register(EpisodeTitle)
+admin.site.register(EpisodeVersion)
