0c620fe9fd15c8001e1279299fee9e50550ef6a7
Jan Guzman <janfrancisco19@gmail.com>
2018-11-19 15:32:04 -0400

    Working with the anime custom form sets

--- Anime/admin.py
+++ Anime/admin.py
@@ -1,10 +1,11 @@
+from django.forms.models import BaseInlineFormSet
 from django.contrib import admin
 from .models import Anime
 from .models import Genre
 from .models import AnimeGenre
 from .models import AnimeDescription
 from .models import Season
-from .models import SeasonAlternativeTitle 
+from .models import SeasonAlternativeTitle
 from .models import Language
 from .models import Episode
 from .models import EpisodeTitle
@@ -12,7 +13,7 @@ from .models import EpisodeVersion
 
 # Register your models here.
 admin.site.register(Language)
-admin.site.register(Anime)
+# admin.site.register(Anime)
 admin.site.register(Genre)
 admin.site.register(AnimeGenre)
 admin.site.register(AnimeDescription)
@@ -21,3 +22,39 @@ admin.site.register(SeasonAlternativeTitle)
 admin.site.register(Episode)
 admin.site.register(EpisodeTitle)
 admin.site.register(EpisodeVersion)
+
+class RequiredInlineFormSet(BaseInlineFormSet):
+  """
+  Generates an inline formset that is required
+  """
+
+  def _construct_form(self, i, **kwargs):
+      """
+      Override the method to change the form attribute empty_permitted
+      """
+      form = super(RequiredInlineFormSet, self)._construct_form(i, **kwargs)
+      form.empty_permitted = False
+      return form
+
+"""
+#
+# Anime creation admin view related
+#
+"""
+class AnimeGenreInline(admin.StackedInline):
+    model = AnimeGenre
+    extra = 1
+    max_num = 1
+    formset = RequiredInlineFormSet
+
+
+class SeasonInline(admin.StackedInline):
+    model = Season
+    extra = 1
+    max_num = 1
+    formset = RequiredInlineFormSet
+
+
+@admin.register(Anime)
+class AnimeAdmin(admin.ModelAdmin):
+    inlines = [AnimeGenreInline, SeasonInline]
